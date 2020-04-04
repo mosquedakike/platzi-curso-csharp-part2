29cf9c2bac2996b32782c55588540afe012e435f
Quique <enrique_inf@hotmail.com>
2020-04-03 18:25:36 -0600

    agrengando clase AlumnoPromedio

---
+++ Entidades/AlumnoPromedio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreEscuela.Entidades
+{
+    public class AlumnoPromedio
+    {
+        public float promedio;
+
+        public string alumnoid;
+
+        public string alumnoNombre;
+    }
+}
